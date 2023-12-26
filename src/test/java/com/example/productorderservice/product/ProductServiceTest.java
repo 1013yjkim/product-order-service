f3c809b8daf8a34bf458ece38112371a0a312160
1013yjkim <1013yjkim@naver.com>
2023-12-26 17:53:27 +0900

    inner class들 밖으로 뺴주고 코드 깔끔하게 정리

--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -2,10 +2,6 @@ package com.example.productorderservice.product;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.Assert;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class ProductServiceTest {
     private ProductService productService;
@@ -20,85 +16,16 @@ public class ProductServiceTest {
     }
     @Test
     void 상품등록(){
+        final AddProductRequest request = 상품등록요청_생성();
+        productService.addProduct(request);
+    }
+
+    private static AddProductRequest 상품등록요청_생성() {
         final String name = "상품명";
         final int price=1000;
         final DiscountPolicy discountPolicy=DiscountPolicy.NONE;
-        final AddProductRequest request= new AddProductRequest(name, price, discountPolicy);
-        productService.addProduct(request);
-    }
-    private class ProductService{
-        private final ProductPort productPort;
-
-        private ProductService(final ProductPort productPort){
-            this.productPort=productPort;
-        }
-        public void addProduct(final AddProductRequest request){
-            final Product product = new Product(request.name(), request.price(), request.discountPolicy());
-            productPort.save(product);
-        }
-
+        return new AddProductRequest(name, price, discountPolicy);
     }
 
 
-
-
-    private record AddProductRequest(String name, int price, DiscountPolicy discountPolicy) {
-        private AddProductRequest {
-            Assert.hasText(name, "상품명은 필수입니다.");
-            Assert.isTrue(price > 0, "상품가격은 0보다 커야합니다.");
-            Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
-        }
-    }
-    private enum DiscountPolicy{
-        NONE
-    }
-
-    private class Product{
-        private final String name;
-        private final int price;
-        private final DiscountPolicy discountPolicy;
-        private Long id;
-
-        public Product(final String name, final int price, final DiscountPolicy discountPolicy) {
-            this.name = name;
-            this.price = price;
-            this.discountPolicy = discountPolicy;
-            ///validation 체크
-            Assert.hasText(name, "상품명은 필수입니다.");
-            Assert.isTrue(price > 0, "상품가격은 0보다 커야합니다.");
-            Assert.notNull(discountPolicy, "할인 정책은 필수입니다.");
-        }
-        public void assignId(final Long id){
-            this.id=id;
-
-        }
-        public Long getId(){
-            return id;
-        }
-    }
-
-    private interface ProductPort {
-        public void save(final Product product);
-    }
-    private class ProductAdapter implements ProductPort {
-        private final ProductRepository productRepository;
-
-        private ProductAdapter(final ProductRepository productRepository) {
-            this.productRepository = productRepository;
-        }
-
-        @Override
-        public void save(final Product product) {
-            productRepository.save(product);
-        }
-    }
-
-    private class ProductRepository {
-        private Long sequence = 0L;
-        private Map<Long, Product> persistence = new HashMap<>();
-        public void save(final Product product) {
-            product.assignId(++sequence);
-            persistence.put(product.getId(),product);
-        }
-    }
 }
